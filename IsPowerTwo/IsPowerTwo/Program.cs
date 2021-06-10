f4543a8c0fd635150eaf38e61fa212cd92f53ce4
Mikah Kainen <Mikah.Kainen@GMR.local>
2021-06-09 22:10:12 -0700

    Computer Archetecture Stuff

---
+++ IsPowerTwo/IsPowerTwo/Program.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IsPowerTwo
+{
+    public class Program
+    {
+        public static Stack<byte> stack = new Stack<byte>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool answer1 = SecondTry(32);
+            bool answer2 = SecondTry(128);
+            bool answer3 = SecondTry(122);
+        }
+
+
+
+        public static bool IsPowerTwo(int number)
+        {
+            string binary = Convert.ToString(number, 2);
+
+            bool wasOneFound = false;
+            foreach(char bin in binary)
+            {
+                if(bin == '1')
+                {
+                    if(wasOneFound)
+                    {
+                        return false;
+                    }
+                    wasOneFound = true;
+                }
+            }
+            return true;
+
+        }
+
+        public static bool SecondTry(int number)
+        {
+            int wall = 1;
+            while(wall < number)
+            {
+                wall = wall << 1;
+            }
+
+            return wall == number;
+        }
+
+        public static int NearestPowerOf2(int number)
+        {
+            int wall = 1;
+            while (wall <= number)
+            {
+                wall = wall << 1;
+            }
+
+            return wall >> 1;
+        }
+
+        public static int ClosestPowerOf2(int number)
+        {
+            int wall = 1;
+            while(wall <= number)
+            {
+                wall = wall << 1;
+            }
+
+            if(wall - number > number - (wall >> 1))
+            {
+                return wall >> 1;
+            }
+            return wall;
+        }
+
+        public static bool NumericalCheckPowerTwo(int number)
+        {
+            return (int)Math.Log2(number) == Math.Log2(number);
+        }
+
+        public static int MakeZero(int number, int index)
+        {
+            int binaryIndex = 1 << index;
+            return number & (int.MaxValue - binaryIndex);
+        }
+
+        public static int MakeOne(int number, int index)
+        {
+            int binaryIndex = 1 << index;
+            return number | binaryIndex;
+        }
+
+        public static int FlipABit(int number, int index)
+        {
+            int binaryIndex = 1 << index;
+            return number ^ binaryIndex;
+        }
+
+        public static int SetABit(int number, int index, bool shouldOne)
+        {
+            int mask = 1 << index;
+            number |= mask;
+
+            if(shouldOne)
+            {
+                return number;
+            }
+
+            return number - mask;
+        }
+
+        public static byte[] IntToByteArray(int number)
+        {
+            byte[] returnValue = new byte[sizeof(int)];
+            for(int i = 0; i < returnValue.Length; i ++)
+            {
+                returnValue[i] = GetByte(number, i);
+            }
+
+            return returnValue;
+        }
+
+        //public static byte GetAByte(int number, int index)
+        //{
+        //    int modFactor = (int)Math.Pow(10, index + 1);
+
+        //    return (byte)(number % modFactor / (modFactor/10));
+        //}
+
+        public static byte GetByte(int number, int index)
+        {
+            int shiftLength = index * 8;
+            int targetByte = number >> shiftLength;
+            int mask = 255;
+
+            return (byte)(targetByte & mask);
+        }
+
+        public unsafe static bool IsPalindrome(string word)
+        {
+            fixed(char* location = word)
+            {
+                char* current = location;
+                char* target = current + word.Length;
+                int currentIndex = word.Length - 1;
+                while(current < target)
+                {
+                    if(*current != word[currentIndex])
+                    {
+                        return false;
+                    }
+                    currentIndex--;
+                    current++;
+                }
+            }
+
+            return true;
+        }
+
+        enum Commands
+        {
+            Add = 0x0,
+            Sub = 0x1,
+            Mul = 0x2,
+            Div = 0x3,
+            Mod = 0x4,
+        };
+
+        public static int DoMath(int data)
+        {
+
+            byte[] info = IntToByteArray(data);
+
+            int returnValue;
+
+            int command = info[3];
+
+            switch((Commands)command)
+            {
+                case Commands.Add:
+                    returnValue = info[1] + info[0];
+                    break;
+
+                case Commands.Sub:
+                    returnValue = Subtract(info[1], info[0]);
+                    break;
+
+                case Commands.Mul:
+                    returnValue = Multiply(info[1], info[0]);
+                    break;
+
+                case Commands.Div:
+                    returnValue = Divide(info[1], info[0]);
+                    break;
+
+                case Commands.Mod:
+                    returnValue = Mod(info[1], info[0]);
+                    break;
+
+                default:
+                    throw new Exception("That command does not exist");
+                    break;
+            }
+
+            return returnValue;
+        }
+
+
+        public static byte Multiply(byte left, byte right)
+        {
+            byte returnValue = 0;
+            for (int i = 0; i < right; i++)
+            {
+                returnValue += left;
+            }
+            return returnValue;
+        }
+
+        public static byte Subtract(byte left, byte right)
+        {
+            return (byte)(left + -1 *right);
+        }
+
+        public static byte Divide(byte left, byte right)
+        {
+            byte currentNum = left;
+            byte currentIterations = 0;
+            while(currentNum >= right)
+            {
+                currentIterations++;
+                currentNum -= right;
+            }
+            return currentIterations;
+        }
+
+        public static byte Mod(byte left, byte right)
+        {
+            byte currentNum = left;
+            while (currentNum >= right)
+            {
+                currentNum -= right;
+            }
+            return currentNum;
+        }
+
+
+
+        public static void SMath(byte command)
+        {
+            ~~
+
+        }
+
+
+        public static void SAdd()
+        {
+            stack.Push((byte)(stack.Pop() + stack.Pop()));
+        }
+
+        public static void SSub()
+        {
+            stack.Push((byte)Subtract(stack.Pop(), stack.Pop()));
+        }
+
+        public static void SMult()
+        {
+            byte left = stack.Pop();
+            byte right = stack.Pop();
+            byte returnValue = 0;
+            for (int i = 0; i < right; i++)
+            {
+                returnValue += left;
+            }
+            stack.Push(returnValue);
+        }
+
+        public static void SDiv()
+        {
+            byte left = stack.Pop();
+            byte right = stack.Pop();
+            byte currentNum = left;
+            byte currentIterations = 0;
+            while (currentNum >= right)
+            {
+                currentIterations++;
+                currentNum -= right;
+            }
+            stack.Push(currentIterations);
+        }
+
+        public static void SMod()
+        {
+            byte left = stack.Pop();
+            byte right = stack.Pop();
+            byte currentNum = left;
+            while (currentNum >= right)
+            {
+                currentNum -= right;
+            }
+            stack.Push(currentNum);
+        }
+    }
+}
---
+++ IsPowerTwo/UnitTest/UnitTest1.cs
@@ -0,0 +1,232 @@
+
+using IsPowerTwo;
+
+using Xunit;
+
+namespace UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+        [Theory]
+        [InlineData(64)]
+        [InlineData(3)]
+        public void DoesIsPowerOf2Work(int number)
+        {
+            Assert.Equal(Program.IsPowerTwo(number), Program.NumericalCheckPowerTwo(number));
+        }
+
+
+        [Theory]
+        [InlineData(64)]
+        [InlineData(3)]
+        public void DoesSecondTryWork(int number)
+        {
+            Assert.Equal(Program.SecondTry(number), Program.NumericalCheckPowerTwo(number));
+        }
+
+        [Theory]
+        [InlineData(64)]
+        [InlineData(32)]
+        public void IsPower2(int number)
+        {
+            Assert.True(Program.NumericalCheckPowerTwo(number));
+        }
+
+        [Theory]
+        [InlineData(3)]
+        [InlineData(57)]
+        public void IsNotPower2(int number)
+        {
+            Assert.False(Program.NumericalCheckPowerTwo(number));
+        }
+
+        [Theory]
+        [InlineData(4, 4)]
+        [InlineData(3, 2)]
+        [InlineData(255, 128)]
+        [InlineData(0, 0)]
+        [InlineData(257, 256)]
+        public void DoesNearestPowerOf2Work(int number, int expectedResult)
+        {
+            Assert.Equal(expectedResult, Program.NearestPowerOf2(number));
+        }
+
+        [Theory]
+        [InlineData(4, 4)]
+        [InlineData(3, 4)]
+        [InlineData(255, 256)]
+        [InlineData(0, 0)]
+        [InlineData(257, 256)]
+        [InlineData(13, 16)]
+
+        public void DoesClosestPowerOf2Work(int number, int expectedResult)
+        {
+            Assert.Equal(expectedResult, Program.ClosestPowerOf2(number));
+        }
+
+        [Theory]
+        [InlineData(255, 0, 254)]
+        [InlineData(0, 1, 0)]
+        [InlineData(255, 2, 251)]
+        [InlineData(255, 3, 247)]
+
+        public void DoesMakeZeroWork(int number, int index, int expectedResult)
+        {
+            Assert.Equal(expectedResult, Program.MakeZero(number, index));
+        }
+
+        [Theory]
+        [InlineData(0, 0, 1)]
+        [InlineData(255, 1, 255)]
+        [InlineData(0, 2, 4)]
+        [InlineData(0, 3, 8)]
+
+        public void DoesMakeOneWork(int number, int index, int expectedResult)
+        {
+            Assert.Equal(expectedResult, Program.MakeOne(number, index));
+        }
+
+        [Theory]
+        [InlineData(255, 0, 254)]
+        [InlineData(0, 1, 2)]
+        [InlineData(0, 6, 64)]
+        [InlineData(64, 7, 192)]
+
+        public void DoesFlipDigit(int number, int index, int expectedResult)
+        {
+            Assert.Equal(expectedResult, Program.FlipABit(number, index));
+        }
+
+
+        //[Theory]
+        //[InlineData(1, 0, 1)]
+        //[InlineData(20, 1, 2)]
+        //[InlineData(54321, 2, 3)]
+
+        //public void DoesGetAByteWork(int number, int index, int expectedResult)
+        //{
+        //    Assert.Equal(expectedResult, Program.GetAByte(number, index));
+        //}
+
+        [Theory]
+        [InlineData(0x01, 0, 0x01)]
+        [InlineData(0x20, 0, 0x20)]
+        [InlineData(0x54321, 1, 0x43)]
+        [InlineData(0x123987, 2, 0x12)]
+
+        public void DoesGetByteWork(int number, int index, int expectedResult)
+        {
+            Assert.Equal(expectedResult, Program.GetByte(number, index));
+        }
+
+
+        [Theory]
+        [InlineData(0x1111, new byte[]{0x11, 0x11, 0x00, 0x00})]
+        [InlineData(0x123456, new byte[]{0x56, 0x34, 0x12, 0x00})]
+        [InlineData(0x1, new byte[]{0x1, 0x00, 0x00, 0x00})]
+
+        public void DoesIntToByteArrayWork(int number, byte[] expectedResult)
+        {
+            Assert.Equal(expectedResult, Program.IntToByteArray(number));
+        }
+
+
+        [Theory]
+        [InlineData(2, 4)]
+        [InlineData(35, 2)]
+        [InlineData(0, 100)]
+        [InlineData(5, 5)]
+        public void DoesMultiplyWork(byte left, byte right)
+        {
+            Assert.Equal(Program.Multiply(left, right), left * right);
+        }
+
+
+        [Theory]
+        [InlineData(2, 4)]
+        [InlineData(35, 2)]
+        [InlineData(0, 100)]
+        [InlineData(5, 5)]
+        public void DoesSubtractWork(byte left, byte right)
+        {
+            Assert.Equal(Program.Subtract(left, right), left - right);
+        }
+
+
+        [Theory]
+        [InlineData(2, 4)]
+        [InlineData(35, 2)]
+        [InlineData(0, 100)]
+        [InlineData(5, 5)]
+        public void DoesDivideWork(byte left, byte right)
+        {
+            Assert.Equal(Program.Divide(left, right), left / right);
+        }
+
+        [Theory]
+        [InlineData(2, 4)]
+        [InlineData(35, 2)]
+        [InlineData(0, 100)]
+        [InlineData(5, 5)]
+        public void DoesModWork(byte left, byte right)
+        {
+            Assert.Equal(Program.Mod(left, right), left % right);
+        }
+
+        [Fact]
+        public void DoesDoMathWork()
+        {
+            Assert.Equal(0xB, Program.DoMath(0x00000506));
+            Assert.Equal(0x2, Program.DoMath(0x01000402));
+            Assert.Equal(0xA, Program.DoMath(0x02000502));
+            Assert.Equal(0x3, Program.DoMath(0x03000602));
+            Assert.Equal(0x1, Program.DoMath(0x04000703));
+        }
+
+        [Fact]
+
+        public void DoesDoMathStackWork()
+        {
+
+        }
+
+
+        [Theory]
+        [InlineData("abcba", true)]
+        [InlineData("y", true)]
+        [InlineData("bro", false)]
+
+        public void DoesIsPalindromeWork(string data, bool expected)
+        {
+            Assert.Equal(expected, Program.IsPalindrome(data));
+        }
+
+        /*
+
+
+        set r1 0x0
+        set r2 0x1
+        set r3 0xA
+
+        1
+        add r1 r1 r2
+
+        eq r5 r1 r3
+        breq 2 r5
+        goto 1
+
+        2
+
+        end
+        goto end
+
+        */
+
+    }
+}
